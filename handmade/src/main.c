48361f48cfb7a9fe43b11eff9b44a08f26d72f26
George Hotz <geohot@gmail.com>
2026-04-02 15:15:01 +0800

    works

--- handmade/src/main.c
+++ handmade/src/main.c
@@ -191,16 +191,16 @@ static void handle_usb_control(void) {
       handle_get_descriptor(wValH, wValL, wLenL);
     } else if (bmReq == USB_SETUP_DIR_HOST_TO_DEV && bReq == USB_REQ_SET_CONFIGURATION) {
       // enable USB bulk mode (bypass MSC)
-      //REG_USB_MSC_CFG = 0x00;
+      REG_USB_MSC_CFG = 0x00;
       // enable bulk endpoint (without the clear in, it'll get a spurious IN, without the clear out, it'll miss an out)
       REG_USB_EP_CFG2 = USB_EP_CFG2_CLEAR_IN;
       REG_USB_EP_CFG2 = USB_EP_CFG2_CLEAR_OUT;
       // receive to 0x911B
       //REG_USB_BULK_EP_CMD = USB_BULK_EP_CMD_CBW;
       // receive to 0x7000
-      //REG_USB_EP_CFG2 = USB_EP_CFG2_ARM_OUT;
+      REG_USB_EP_CFG2 = USB_EP_CFG2_ARM_OUT;
       // setup UAS mode
-      REG_USB_STATUS = USB_STATUS_DMA_READY;
+      //REG_USB_STATUS = USB_STATUS_DMA_READY;
       send_zlp_ack();
       uart_puts("[*** SET CONFIG ***]\n");
     } else if (bmReq == (USB_SETUP_DIR_DEV_TO_HOST | USB_SETUP_TYPE_VENDOR) && bReq == 0xE4) {
